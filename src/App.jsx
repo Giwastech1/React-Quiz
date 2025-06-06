d6b35ac147559856c71c648b34f43f4aa7fd56a8
Giwa Toheeb <giwatoheeb27@gmail.com>
2025-06-06 01:55:12 +0100

    create static UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = {
+  questions: [],
+  status: "loading"
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, question: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "failed" };
+    default:
+      throw new Element("Error");
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status } = state;
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
-  },[])
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question</p>
+        {status === "loading" && <Loader />}
+        {status === "failed" && <Error />}
+        {status === "ready"&& <StartScreen />}
       </Main>
     </div>
   )
